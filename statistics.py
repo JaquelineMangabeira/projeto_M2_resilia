b4bde4f8cc4e1a0f15a7c8172f07b05f48843391
jonathaslkc <jonathaslkc@hotmail.com>
2021-12-14 09:24:52 -0300

    Send of files for implemetation module -statistics

--- statistics.py
+++ statistics.py
@@ -13,16 +13,7 @@ def print_format(txt):
     print('-=' * len_txt)
 
 
-# def view_statistics(id_p, name_p, win_p, lose_p):
-#    print_format('PAINEL ESTATÌSTICO SCORE')
-#    print(f'{"":<2}{"ID":<8}{"NOME":<25}{"Win|Los":<10}')
-#    print('--' * 23)
-#    for index in range(len(id_p)):
-#        print(f'{"+":<2}{id_p[index]:<8}{name_p[index]:<25}{str(win_p[index]) + " -- " + str(lose_p[index]):<9}{"+":<1}')
-#    print('--' * 23)
-
-
-def view_statistics2(d_p): # Function for visualization from the datas of game
+def view_statistics2(d_p):  # Function for visualization from the datas of game
     print_format('PAINEL ESTATÌSTICO SCORE')
     print(f'{"":<2}{"ID":<8}{"NOME":<25}{"Win|Los":<10}')
     print('--' * 23)
@@ -32,13 +23,12 @@ def view_statistics2(d_p): # Function for visualization from the datas of game
 
 
 # Data application for test
-ident = [111, 222, 333, 444]
+
+'''ident = [111, 222, 333, 444]
 names = ['Brunna', 'Jaqueline', 'Laion', 'Jonathas']
 win = [5, 5, 3, 2]
 lose = [0, 0, 1, 2]
-dic = {'ident': ident, 'names': names, 'win': win, 'lose': lose}
+dic = {'ident': ident, 'names': names, 'win': win, 'lose': lose}'''
 
-# invocation the "def" function for test
-# view_statistics(ident, names, win, lose)
 view_statistics2(dic)
 
