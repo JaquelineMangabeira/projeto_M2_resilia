ba5c74fb6f75904d73eb9469ac8543fb6674f393
jonathaslkc <jonathaslkc@hotmail.com>
2021-12-14 13:18:37 -0300

    Definition from model for implemetation

--- statistics.py
+++ statistics.py
@@ -4,7 +4,7 @@
 # Curso de Data Analytics - Resília
 
 
-# Definiction of function for módule visualization --statistics
+# Definiction of function for module visualization --statistics
 
 def print_format(txt):
     len_txt = len(txt)
@@ -17,18 +17,10 @@ def view_statistics2(d_p):  # Function for visualization from the datas of game
     print_format('PAINEL ESTATÌSTICO SCORE')
     print(f'{"":<2}{"ID":<8}{"NOME":<25}{"Win|Los":<10}')
     print('--' * 23)
-    for ind in range(len(d_p['win'])):
-        print(f'{"+":<2}{d_p["ident"][ind]:<8}{d_p["names"][ind]:<25}{str(d_p["win"][ind]) + " -- " + str(d_p["lose"][ind]):<9}{"+":<1}')
+    for ind in d_p:
+        print(f'{"+":<2}{ind["ident"]:<8}{ind["names"]:<25}{str(ind["win"]) + " -- " + str(ind["lose"]):<9}{"+":<1}')
     print('--' * 23)
 
 
 # Data application for test
-
-'''ident = [111, 222, 333, 444]
-names = ['Brunna', 'Jaqueline', 'Laion', 'Jonathas']
-win = [5, 5, 3, 2]
-lose = [0, 0, 1, 2]
-dic = {'ident': ident, 'names': names, 'win': win, 'lose': lose}'''
-
-view_statistics2(dic)
-
+# view_statistics2([{'ident': 0, 'names': 'nome', 'win': 0, 'lose': 0}, {'ident': 1, 'names': 'nome2', 'win': 0, 'lose': 2}, {'ident': 3, 'names': 'nome3', 'win': 1, 'lose': 0}])
