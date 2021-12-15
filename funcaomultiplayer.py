47369afaa269fa79db6265dafca6995846a1e1d7
jonathaslkc <jonathaslkc@hotmail.com>
2021-12-15 15:11:43 -0300

    modified in the lines codig

--- funcaomultiplayer.py
+++ funcaomultiplayer.py
@@ -4,6 +4,13 @@ from threading import Thread
 from funcoesEsteticas import *
 from menusNavegacao import *
 
+######################### CREATE LIST FOR MODULE STATISTICS
+import statistics as s
+aux_list = list()
+control_list = list()
+jgReinicio = list()
+######################### MODULESTATISTIC
+
 jogadores = []
 podio = [[], []]   #indice 0 - ganhadores - indice 1 perdedores
 marcador = ' __ '
@@ -15,9 +22,7 @@ cores = ['\033[1;31m','\033[1;41m','\033[1;32m','\033[1;42m','\033[1;33m','\033[
 
 
 def palavraSecreta():          # sortear palavras secretas
-    listaPalavras = ["boneco de neve", "papal noel", "treno", "sinos", "estrela de belem", "presepio", "pinheiro",
-    "guirlanda", "advento", "panetone", "ceia", "tres reis magos", "uvas passas", "pave ou pra comer", "chamine",
-    "luzes de natal,", "presentes", "carta", "bolas de natal", "feliz natal", "arvore de natal"] 
+    listaPalavras = ["bom", 'teste', 'hoje', "dia"]
     
     palavra_secreta = str(listaPalavras[randint(0,(len(listaPalavras)-1))]).upper() #a palavra secreta é sorteada da lista                                                  
     return palavra_secreta
@@ -35,6 +40,7 @@ def letrasAcertadas(palavra):         # função para mostrar as letras de forma
 
 
 def definirJogadores():      # define quantos jogadores irão jogar e atribui a cada um deles - uma palavra, a qttd de erros e uma cor
+    global jgReinicio
     print()
     numeroJogadores = int(input('Quantas pessoas irão jogar? '))
     if numeroJogadores >20:      #limtar a 20 jogadores 
@@ -50,8 +56,55 @@ def definirJogadores():      # define quantos jogadores irão jogar e atribui a 
         cor = (cores[randint(0,(len(cores)-1))])   #soteia uma cor por jogador
         jogadores[i].update(cor = cor)               #adiciona a cor ao dict
         cores.remove(cor) #para não repetir as cores
-        
+
+        ######################### ADD INDEX LIST OF DICT AND FEEDING THE CONTROL FOR MODULE STATISTICS
+        aux_list.append({'ident': len(aux_list), 'names': jogadores[i]['nome'], 'win': 0, 'lose': 0})
+        control_list.append(jogadores[i]['nome'])
+        jgReinicio = jogadores[:]
+        ######################### MODULESTATISTICS
     return jogadores
+
+################3
+
+def definirJogadores2():  # define quantos jogadores irão jogar e atribui a cada um deles - uma palavra, a qttd de erros e uma cor
+    global jgReinicio
+    print()
+    jogadores = jgReinicio[:]
+    for i, valor_dicio in enumerate(jgReinicio):
+        jogadores[i].update(erros=0)  # cria um dict e add a chave erros
+        jogadores[i].update(palavra=palavraSecreta())  # adiciona a palavra secreta ao dict
+        jogadores[i].update(letrasAcertadas=letrasAcertadas(jogadores[i]['palavra']))  # adiciona as letras acertadas ao dict
+        # print(jogadores)
+    ######################### ADD INDEX LIST OF DICT AND FEEDING THE CONTROL FOR MODULE STATISTICS
+    aux_list.append({'ident': len(aux_list), 'names': jogadores[i]['nome'], 'win': 0, 'lose': 0})
+    control_list.append(jogadores[i]['nome'])
+    jgReinicio = jogadores[:]
+    ######################### MODULESTATISTICS
+    return jogadores
+
+################
+
+
+
+def reiniciaJogo():
+    global vez, jogadores
+    print('1 - Reiniciar Partida')
+    print('2 - Reiniciar Jogo')
+    print('3 - Estatísiticas')
+    print('4 - Voltar para Menu Principal')
+    opc = int(input('Digite sua opcao: '))
+    if opc == 1:
+        jogadores = jgReinicio
+        vez = 0
+        definirJogadores2()
+        inicioJogo()
+    if opc == 2:
+        vez = 0
+        rodarJogo()
+    if opc == 3:
+        s.view_statistics2(aux_list)
+    if opc == 4:
+        print()
 
 
 def rodarJogadores():           # para rodar os jogadores caso perde ou ganha
@@ -70,7 +123,7 @@ def tentativa():
     return chute
 
 def imprimePodio():
-
+    global podio
     if len(podio[0]) == 0:
         print('Não houveram ganhadores')
     else:
@@ -79,6 +132,12 @@ def imprimePodio():
             print(f'{colocacao}º lugar', end= ' - ')
             print(i)
             colocacao += 1
+
+    ######################### LAUCH INFORMATION AND INVOCATION OF FUNCTION STATISTICS2
+    podio = [[], []]
+    print('\n' * 2)
+    reiniciaJogo()
+    ######################### MODULESTATISTICS
 
 def acertouLetra(tentativa):    
         index = 0
@@ -96,6 +155,11 @@ def acertouLetra(tentativa):
             ganhador = jogadores[vez]['nome']
             passouDeFase(f'{ganhador} acertou a palavra')
             podio[0].append(jogadores[vez]['nome'])                     #adiciona o nome ao pódio
+
+            ######################### UPDATE VALUE IN KEY -WIN- FROM THE DICT FOR MODULE STATISTICS
+            aux_list[control_list.index(jogadores[vez]['nome'])]['win'] += 1
+            ######################### MODULESTATISTICS
+
             jogadores.remove(jogadores[vez])                        #remove vencedor da lista de jogadores
             if len(jogadores) > 0:
                 rodarJogadores()
@@ -112,6 +176,11 @@ def errouLetra():
         perdeu(f'{jogadores[vez]["nome"]} foi enforcado. A palavra era {jogadores[vez]["palavra"] }')          
         
         podio[1].append(jogadores[vez]['nome'])  #adiciona o nome ao pódio
+
+        ######################### UPDATE VALUE IN KEY -LOSE- FROM THE DICT FOR MODULE STATISTICS
+        aux_list[control_list.index(jogadores[vez]['nome'])]['lose'] += 1
+        ######################### MODULESTATISTICS
+
         jogadores.remove(jogadores[vez]) #remove perdedor da lista de jogadores
         
         if len(jogadores) == 0:
